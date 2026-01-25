adf3b3f14364b3e7be0206af86aab1103febf407
Fabio-Bianco <biancobfabio@gmail.com>
2026-01-25 20:10:18 +0100

    feat: render UsersList component alongside PostsList in App

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,20 @@
 // Questo è il componente che vogliamo mostrare quando l’app parte.
 import PostsList from "./pages/PostsList";
 
+// Importiamo anche il componente della pagina utenti 
+import UsersList from "./pages/UsersList";
+
 // App è il componente root dell’applicazione.
 // Tutta l’app React parte da qui.
 function App() {
-
-    // Qui diciamo: “Quando React renderizza App,
-  // deve renderizzare il componente PostsList”.
-  return <PostsList />;
+  return (
+    <div>
+      <PostsList />
+      <UsersList />
+    </div>
+  );
 }
+
 
 // Rendiamo App disponibile al resto dell’app (main.tsx lo userà)
 export default App;
