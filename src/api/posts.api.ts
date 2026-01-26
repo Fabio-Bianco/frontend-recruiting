59ac99def0062aeda686a83f9912ed7794e92f14
Fabio-Bianco <biancobfabio@gmail.com>
2026-01-26 11:47:02 +0100

    feat: implement getPostById function and enhance PostsDetails component to display post details

--- src/api/posts.api.ts
+++ src/api/posts.api.ts
@@ -3,11 +3,15 @@ import type { Post } from "../types/post";
 
 // Funzione che recupera tutti i post dal backend
 export async function getPosts(): Promise<Post[]> {
-
     // Chiamata HTTP GET verso /posts
   const response = await apiClient.get<Post[]>("/posts");
-
   //Ritorniamo solo i dati JSON
   return response.data;
 
+}
+
+// Funzione che recupera un singolo post dato il suo ID
+export async function getPostById(id: string): Promise<Post> {
+  const { data } = await apiClient.get<Post>(`/posts/${id}`);
+  return data;
 }
--- src/pages/PostsDetails.tsx
+++ src/pages/PostsDetails.tsx
@@ -1,17 +1,30 @@
-// importazione useParams da react-router-dom
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-
-
+import { getPostById } from "../api/posts.api";
+import type { Post } from "../types/post";
 
 export default function PostsDetails() {
+  const { id } = useParams();
+  const [post, setPost] = useState<Post | null>(null);
 
-    const { id } = useParams();
+  useEffect(() => {
+    async function loadPost() {
+      if (!id) return;                // protezione: id potrebbe mancare
+      const data = await getPostById(id);
+      setPost(data);
+    }
+    loadPost();
+  }, [id]);
 
-    return (
-        <div>
-            <h1>Pagina di dettaglio del post</h1>
-            <p>ID del post: {id}</p>
-        </div>
-    )
+  if (!post) return <p>Caricamento...</p>;
 
+  return (
+    <div>
+      <h1>Pagina di dettaglio del post</h1>
+      <p>ID del post: {id}</p>
+      <p>Titolo: {post.title}</p>
+      <p>Contenuto: {post.content}</p>
+      <small>Creato il: {post.createdAt}</small>
+    </div>
+  );
 }
